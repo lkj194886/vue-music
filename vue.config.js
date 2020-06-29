4527b27cedfd84b48018de31b871ae35ebc04314
wgg <1748495608@qq.com>
2020-06-29 16:12:50 +0800

    修改

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
     devServer: {
         disableHostCheck: true,
+        host: "0.0.0.0",
+        port: 8090,
+        https: false,
+        hotOnly: false, // See https://github.com/vuejs/vue-cli/blob/dev/docs/cli-service.md#configuring-proxy
+        proxy: null // string | Object
+        
     }
 }
