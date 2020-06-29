6232d50cf9575029cdf6e8907c3bf71429e68ad7
wgg <1748495608@qq.com>
2020-06-29 16:04:43 +0800

    update

--- config/index.js
+++ config/index.js
@@ -38,7 +38,7 @@ module.exports = {
     devServer: {
       open: process.platform === "darwin",
       disableHostCheck: true,
-      host: "0.0.0.0",
+      host: "weigg.top",
       port: 8090,
       https: false,
       hotOnly: false, // See https://github.com/vuejs/vue-cli/blob/dev/docs/cli-service.md#configuring-proxy
